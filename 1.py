4acdb8a86853a4f1480122b306ad1091a4e39ddc
lion2000-cmd <64172943+lion2000-cmd@users.noreply.github.com>
2020-04-30 15:58:26 -0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+print "hola mundo"
+nombre = raw_input("como te llamas?")
+edad = raw_input("cual es tu edad?")
+edad= int (edad)
+print "tu nombre es: %s" %(nombre)
+
+if edad>=18:
+    print"eres mayor"
+else:
+    print"eres menor"
+    
