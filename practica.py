c0f47d2ea805611bf6d5afd06bed0c1c74830a6e
lion2000-cmd <64172943+lion2000-cmd@users.noreply.github.com>
2020-04-30 16:17:29 -0500

    Add files via upload

---
+++ practica.py
@@ -0,0 +1,12 @@
+palabra ="hola"
+for letra in palabra:
+    print letra
+frutas=["manzana","pera","mango"]
+for fruta in frutas:
+    print frutas
+rango = range(0,10)
+print rango
+for i in rango:
+    print i 
+
+    
